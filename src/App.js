411470203b04816a21e716ff06c5d2507db22393
Sum-RED <Sum-RED@.noreply.github.com>
2021-01-06 23:35:12 -0500

    react configuration

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Navbar, NavbarBrand } from "reactstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
+    </div>
+  );
+}
+
+export default App;
