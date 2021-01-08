481b2042e97be016da7471675f64c531ade9e294
Sum-RED <Sum-RED@.noreply.github.com>
2021-01-08 00:56:26 -0500

    Components 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import Menu from "./components/MenuComponent";
 import { Navbar, NavbarBrand } from "reactstrap";
 
 function App() {
@@ -9,6 +9,7 @@ function App() {
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
