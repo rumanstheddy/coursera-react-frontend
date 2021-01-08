a88d71a7519f7f014cac6e5a2c42e24f8545ac79
Sum-RED <Sum-RED@.noreply.github.com>
2021-01-08 01:08:56 -0500

    components 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
+import React, { Component } from "react";
 import Menu from "./components/MenuComponent";
 import { Navbar, NavbarBrand } from "reactstrap";
+import { DISHES } from "./shared/dishes";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        {console.log(this.state.dishes)}
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
