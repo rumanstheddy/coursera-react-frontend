1df6300388cf883b376247406637535630ff7d56
Sum-RED <Sum-RED@.noreply.github.com>
2021-01-13 11:27:10 -0500

    React Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
 import Menu from "./MenuComponent";
-import DishDetail from "./DishdetailComponent";
+import Home from "./HomeComponent";
 import { DISHES } from "../shared/dishes";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 class Main extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       dishes: DISHES,
-      selectedDish: null,
     };
   }
 
@@ -19,20 +20,23 @@ class Main extends Component {
   }
 
   render() {
+    const HomePage = () => {
+      return <Home />;
+    };
     return (
       <div className="container">
         <Header />
-        <Menu
-          dishes={this.state.dishes}
-          onClick={(dishId) => this.onDishSelect(dishId)}
-        />
-        <DishDetail
-          dish={
-            this.state.dishes.filter(
-              (dish) => dish.id === this.state.selectedDish
-            )[0]
-          }
-        />
+
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route
+            exact
+            path="/menu"
+            // Creating a functional component inline to pass props
+            component={() => <Menu dishes={this.state.dishes} />}
+          />
+          <Redirect to="/home" />
+        </Switch>
         <Footer />
       </div>
     );
